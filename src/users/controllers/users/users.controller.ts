86d927ec1bed3bc77d0ca247ee9ed339c461fe3b
tuan-khuat <tuan.khuat@corize.co.jp>
2022-10-19 10:00:28 +0900

    add user services

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,18 +1,38 @@
 import { CreateUserDto } from './../../dtos/CreateUser.dto';
-import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
-import { Request, Response } from 'express';
-import { Console } from 'console';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  UsePipes,
+  ValidationPipe,
+  Req,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
+  //   @Get()
+  //   getUsers() {
+  //     return { username: 'Tuan', email: 'tuan.khuat@corize.co.jp' };
+  //   }
+
   @Get()
-  getUsers() {
+  getUserSortBy(@Query('sortBy') sortBy: string) {
+    console.log(sortBy);
     return { username: 'Tuan', email: 'tuan.khuat@corize.co.jp' };
   }
 
-  @Post()
+  @Post('create')
+  @UsePipes(new ValidationPipe())
   createUser(@Body() userData: CreateUserDto) {
-    console.log(userData.email);
+    console.log(userData);
     return [];
+  }
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    console.log({ id });
+    return { id };
   }
 }
